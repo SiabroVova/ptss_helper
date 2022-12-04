5459701fac9dbe847519dc8d44eed351592b6f45
stanchikzt <stanchikzt@gmail.com>
2022-12-04 11:15:02 +0200

    Змінено росташування звукових файлів Додано головний екран MainScreen Змінено App на MDApp Додано визначення платформи для змінення розмірів вікна. Додано теми theme_cls.theme_style Додано theme_cls.material_style для гарнішого вигляду у майбутньому. Додано методи - додавання кнопки на екран center_button - при натисканні запуск гейм add_game_widget Додано requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from kivy.app import App
+from kivymd.app import MDApp
+from kivy import Config
+from kivy.utils import platform
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
+from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.screen import MDScreen
+
+if platform not in ('android', 'ios'):
+    Config.set('graphics', 'resizable', '0')
+    Window.size = (400, 800)
 
 
 class EmdrBall(Widget):
+
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
@@ -18,6 +28,7 @@ class EmdrBall(Widget):
 
 
 class EmdrPlace(Widget):
+
     ball = ObjectProperty(None)
 
     def serve_ball(self, vel=(3, 3)):
@@ -30,22 +41,46 @@ class EmdrPlace(Widget):
         # bounce ball off bottom or top
         if (self.ball.y < self.y) or (self.ball.top > self.top):
             self.ball.velocity_y *= -1
-            sound = SoundLoader.load('boop.wav')
+            sound = SoundLoader.load('sound/boop.wav')
             sound.play()
 
         # bounce ball off left or right
         if (self.ball.x < self.x) or (self.ball.right > self.right):
             self.ball.velocity_x *= -1
-            sound = SoundLoader.load('boop.wav')
+            sound = SoundLoader.load('sound/boop.wav')
             sound.play()
 
 
-class EmdrApp(App):
+class MainScreen(MDScreen):
+    pass
+
+
+class EmdrApp(MDApp):
+
+    def __init__(self, **kwargs):
+        super(EmdrApp, self).__init__(**kwargs)
+        self.main_screen = MainScreen()
+
     def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.material_style = "M3"
+        return self.main_screen
+
+    def center_button(self):
+        s_button = MDRectangleFlatButton(text="Натисніть для старту",
+                                         on_release=self.add_game_widget)
+        s_button.pos_hint = {"center_x": 0.5, "center_y": 0.5}
+        return s_button
+
+    def add_game_widget(self, dt=0):
+        self.main_screen.clear_widgets()
         game = EmdrPlace()
         game.serve_ball()
+        self.main_screen.add_widget(game)
         Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
+
+    def on_start(self):
+        self.main_screen.add_widget(self.center_button())
 
 
 if __name__ == '__main__':
